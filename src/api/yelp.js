f032c3c331ee42bf2329ed23d9ea6aff6978a558
Eddy Garcia <32507458+kirizhu@users.noreply.github.com>
2020-12-06 10:59:58 +0100

    Update yelp.js

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: 'https://api.yelp.com/v3/businesses',
   headers: {
     Authorization:
-      'Bearer A29qfXYoTftS-VtxuxymThmrx5aymBdfCbun0fnXKBlEIvAUPU_iq1Yn5IglXkQsNd1JOfXV0wq37RTkE8K3cgmbU0DiiDtFAI94kZAVf46siUVzX5nx02Mj35R9X3Yx',
+      'Bearer YOURAPIKEY',
   },
 });
